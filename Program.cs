05b23d278b950e7e347189de18325a7d173e780b
Gibson Patterson <Pattersongibson@gmail.com>
2020-06-25 14:08:14 -0600

    if and else

--- Program.cs
+++ Program.cs
@@ -2,17 +2,23 @@
 
 namespace c_sharp_intro
 {
-    class Program
-    {
-        static void Main(string[] args)
-
-        Console.Writeline( "Please tell us if you want to add or substract: (add/subtract") );
     
-        string userOperator == Console.ReaLine();
-        
-        string userOperator == add" )
+     static void .Main(string[] args)
+        {
+            // It is a good idea, for user experience, to prompt the user.
+            // Guiding the user is great, as it tells them what is expected.
+            Console.WriteLine( "Please enter a string:" );
+            string userInput = Console.ReadLine(); // Assignment; collecting data from the user.
+            Console.WriteLine( "Please tell us if you want to add or subtract: (add/subtract)" );
+            string userOperator = Console.ReadLine();
 
-         Console.WriteLine( "The user entered a value." );
+            // We can use "if" statements to check a "condition".
+            // If the "if statement" is true, its code block (marked by curly braces {} will execute.)
+            if ( userInput.Length > 0 )
+            // Check if the user typed: add
+            if ( userOperator == "add" )
+            {
+                Console.WriteLine( "The user entered a value." );
             } // If there is an "else" statement proceeding an "if", it will only execute if the "if statement" was false.
             else
             {
@@ -25,15 +31,6 @@ namespace c_sharp_intro
                 // Calculate the result.
                 int result = firstNum + secondNum;
                 Console.WriteLine( "{0} + {1} = {2}", firstNum, secondNum, result );
-
-
-
-        } 
-            
-        
-
-         
-
-
-
-}
+            }
+        }
+    }
