51cf669d6461a3dc6b58022bad90d0dc2a817c2d
Gibson Patterson <Pattersongibson@gmail.com>
2020-06-25 11:45:09 -0600

    Greater than less than

--- Program.cs
+++ Program.cs
@@ -6,78 +6,46 @@ namespace c_sharp_intro
     {
         static void Main(string[] args)
         {
-            string myString = "This is s string!";
-            // integer datatype is used for whole numbers (no decimals.)
-            int myInt = 42;
-            // Double datatype is used for deciemal  point numbers.
-            double myDouble = 33.666;
-            // Bolean is datatype is used for TRUE/FALSE values  - think binary!
-            bool myBool = true;
-
-
-            // Subtraction opertor is " - " (minus)
-            Console.WriteLine( myInt - 10 ); // 32
-
-            // Mulitiplication operator is "*" (multiply.)
-            Console.WriteLine( myInt * 2 ); //84
-
-            // Division operator is "/" ( division.) 
-            Console.WriteLine( myInit / 2 ); // 21
-
-            // Modulus operator is "%" (percent sign.)
-            Console.WriteLine( myInt % 13 ); // 3
-
-            //Addition operator is "+" ( plus.)
-            ?? *** wait!gitThis is ALSO the concatenation operator.
-            Console.WriteLine( myInt + 8 );// 50
-
-            Console.WriteLine("3" +"3"); // 93
-
-            Console.WriteLine("3" +"3"); // 33
-
-            // Output our variable.
-            Console.WriteLine( myInit ) ; // 42
-
-            myInit = + 2; // 44
-            Console.WriteLine( myInit ) ; // 44
-
-            myInit = + 2; 
-            Console.WriteLine( myInt );
-
-            // myInit = myInit + "2"; // We can't turn Init inot a string!
-                                     // We told C# that myInit would be always be an Int!
-
-            // Console.WriteLine( myInit );
-
-            myInt++; // The same typing: myInit = myInit + 1;
-            Console.WriteLine( myInit ); // 45
-
-            MyInit --; // The same as typing : myInit = MyInit - 1;
-            ConsoleModifiers.WriteLine( myInit ); // 44
-
-            myInit += 3; // The same as typing: my Init = myInit +3;
             
+            // Logical operators .
+            Console.WriteLine( true || true ); // is OR
+            Console.WriteLine( false || true ); 
+
+            
+            // AnND will evaluate to te TRUE only if BOTH 
+            Console.WriteLine( true && true ); && is AND
+            Console.WriteLine( false && true ); // True.
+            Console.WriteLine( 2.25 == 2.25 ); // False.
 
 
+           // How does this behhave with different data-types?
+           // Console.WriteLine( true && true 1 ); DOESNT!
 
+            // Comparitive operators ...
+            // "Equal to" check.
+            Console.WriteLine(  false == false ); True.
+            Console.WriteLine(  true == false ); False            
+            Console.WriteLine( 2.25 == 2.25 ); // True.    
+            Console.WriteLine( 2.25 == 3.14 ); // False.
+            // Console.write( 31.4 == "3.14 ); // Cannot check two diff. types!
 
+            // "Greater than" check.
+            Console.WriteLine( 6 < 7 ); // False.
+            Console.WriteLine( 10 < 5 ); // False.
 
+            // "Less than" check
+            Console.WriteLine( 6 > 7 ); // True.
+            Console.WriteLine( 10 > 5 ); // False.
 
-
-
-
-
-
-
-
-
-
+            // "Greater- than-or-equal-to"
+            Console.WriteLine( 5 >= 5 ); // False.
+            Console.WriteLine( 10 > 5 ); // False.
 
 
 
 
             
-            ) 
+        
 
         }   
 
