57618c793703c8ecbf61d889560a0ed3106c7d9f
Gibson Patterson <Pattersongibson@gmail.com>
2020-06-25 13:18:49 -0600

    userinput variables

--- Program.cs
+++ Program.cs
@@ -5,60 +5,26 @@ namespace c_sharp_intro
     class Program
     {
         static void Main(string[] args)
+
+        Console.WriteLine( " Please enter string: ");
+        string userInput + Console.ReadLine();
+
         {
-            
-            // Logical operators .
-            Console.WriteLine( true || true ); // is OR
-            Console.WriteLine( false || true ); 
+        if ( userInput.Length > 0 ) 
 
-            
-            // AnND will evaluate to te TRUE only if BOTH 
-            Console.WriteLine( true && true ); && is AND
-            Console.WriteLine( false && true ); // True.
-            Console.WriteLine( 2.25 == 2.25 ); // False.
+            Console.WriteLine( "The user entered a value. " );
+        } 
+        else
+        {
+            Console.WriteLine("The user did not enter a value. "); 
+        }
 
-
-           // How does this behhave with different data-types?
-           // Console.WriteLine( true && true 1 ); DOESNT!
-
-            // Comparitive operators ...
-            // "Equal to" check.
-            Console.WriteLine(  false == false ); True.
-            Console.WriteLine(  true == false ); False            
-            Console.WriteLine( 2.25 == 2.25 ); // True.    
-            Console.WriteLine( 2.25 == 3.14 ); // False.
-            // Console.write( 31.4 == "3.14 ); // Cannot check two diff. types!
-
-            // "Greater than" check.
-            Console.WriteLine( 6 < 7 ); // False.
-            Console.WriteLine( 10 < 5 ); // False.
-
-            // "Less than" check
-            Console.WriteLine( 6 > 7 ); // True.
-            Console.WriteLine( 10 > 5 ); // False.
-
-            // "Greater- than-or-equal-to"
-            Console.WriteLine( 5 >= 5 ); // False.
-            Console.WriteLine( 10 > 5 ); // False.
-
-            // NOT equal to " check.
-            Console.WriteLine( false != false ); // False.
-            Console.WriteLine( true != false ); // True.
-            Console.WriteLine( 5 != 5 ); // False.
-            Console.WriteLine( 5 != 10 ); // True.
-
-
-
-
-
-
-
-
+        } 
             
         
 
-        }   
+         
 
 
-    }
+
 }
