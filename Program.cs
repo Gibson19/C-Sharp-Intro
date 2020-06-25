1825fe9cac86cbdd06de414984d6c339578e8266
Gibson Patterson <Pattersongibson@gmail.com>
2020-06-25 13:54:59 -0600

    add if and else

--- Program.cs
+++ Program.cs
@@ -12,17 +12,19 @@ namespace c_sharp_intro
         
         string userOperator == add" )
 
-        Console.Writeline( "Pleae enter the first number:" );
-        int firstNum = Int32.Parse( Console.ReadLine() );
-
-        Console.Writeline( "Pleae enter the second number:" );
-        int secondNum = Int32.Parse( Console.ReadLine();
-
-        Int result = firstnum + seconNum;
-        Console.Writeline( "{0} + {1} = {2}",  );
-
-
-
+         Console.WriteLine( "The user entered a value." );
+            } // If there is an "else" statement proceeding an "if", it will only execute if the "if statement" was false.
+            else
+            {
+                Console.WriteLine( "The user did not enter a value." );
+                Console.WriteLine( "Please enter the first number:" );
+                // @link https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+                int firstNum = Int32.Parse( Console.ReadLine() ); // Convert to integer.
+                Console.WriteLine( "Please enter the second number to add:" );
+                int secondNum = Int32.Parse( Console.ReadLine() );
+                // Calculate the result.
+                int result = firstNum + secondNum;
+                Console.WriteLine( "{0} + {1} = {2}", firstNum, secondNum, result );
 
 
 
